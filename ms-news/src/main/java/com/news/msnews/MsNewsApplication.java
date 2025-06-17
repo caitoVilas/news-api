69fdb78c88e51440b7ace69e4c7d3f71b91b290b
caito <caitocd@gmail.com>
2025-06-17 18:56:19 -0300

    first commit

---
+++ ms-news/src/main/java/com/news/msnews/MsNewsApplication.java
@@ -0,0 +1,13 @@
+package com.news.msnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNewsApplication.class, args);
+	}
+
+}
