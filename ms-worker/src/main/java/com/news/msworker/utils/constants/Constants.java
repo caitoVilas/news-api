040abe90238a49e7ba23b69344e09912e6f488b9
caito <caitocd@gmail.com>
2025-06-18 16:22:08 -0300

    config kafka and redis in worker

---
+++ ms-worker/src/main/java/com/news/msworker/utils/constants/Constants.java
@@ -0,0 +1,17 @@
+package com.news.msworker.utils.constants;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Constants class for the application.
+ * This class contains constants used throughout the application.
+ *
+ * @author caito
+ *
+ */
+@UtilityClass
+public class Constants {
+
+    public static final String TOPIC_NAME = "news-topic";
+    public static final String MESAGE_GROUP_NAME = "news-group";
+}
